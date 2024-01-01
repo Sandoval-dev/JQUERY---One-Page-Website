0826b7335d1cd1352dcf17f887105134c9ee4c5c
Osman Erdem <osmankilic10@gmail.com>
2024-01-02 00:06:34 +0300

    One Page Website - 6

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $("#accordion").accordion();
+
+});
+
+$(function() {
+    $("#tabs").tabs();
+});
+
+$(function() {
+ $("#resizable").resizable();
+});
+
+$(function(){
+    $("#dialogButton").click(function() {
+        $("#dialog").dialog();
+    });
+
+
+  
+})
